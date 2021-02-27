46e9724ce548586772787d8147c6f14e2083191c
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-27 00:22:40 -0300

    Adding an button on the center of the screen

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,10 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Icon(
-          Icons.airport_shuttle,
+        child: RaisedButton(
+          onPressed: () {},
+          child: Text('Click me'),
           color: Colors.lightBlue,
-          size: 50.0,
         )
       ),
       floatingActionButton: FloatingActionButton(
