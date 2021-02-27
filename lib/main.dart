1316dc32613609c8456e1596b8c76695687c96f6
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-27 00:10:31 -0300

    Adding an image by the url on the body of the app

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,8 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'Hello world',
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.blueGrey,
-              letterSpacing: 2.0,
-              fontFamily: 'IndieFlower'
-          ),
+        child: Image(
+          image: NetworkImage('https://images.unsplash.com/photo-1505312926838-645f295a20e1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
