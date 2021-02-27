d26547a31193d794c7c34b61ac982756f67b47dc
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-27 00:34:44 -0300

    Making all paddings different from each other

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: 30,
-          vertical: 10
-        ),
+        padding: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
         color: Colors.grey[400],
         child: Text('Hello'),
       ),
